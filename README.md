b487b006b5863e33cd419c81b84d8908b6d3045b
precipice- <configure62@gmail.com>
2017-05-01 19:42:51 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 2017-1 HYU OpenSource Project - Memo Web Application
+
+
+start with nodemon
--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var express = require('express');
-var app = express();
+//index.js
+//http://www.bogotobogo.com/MEAN-Stack/MEAN-Stack-MongoDB-ExpressJS-AngularJS-NodeJS-ToDoList-App.php
+//https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
 
-app.use(express.static(__dirname + '/public'));
+    var express = require('express');
+    var mongoose = require('mongoose');
+    var bodyParser  = require("body-parser");
+    var methodOverride = require("method-override");
+    var app      = express();                        // create our app w/ express
 
-app.listen(3000, function(){
-  console.log('Server On.');
-});
+    // configuration DB ===============================================================
+    mongoose.connect('mongodb://admin:tenpro62@ds113841.mlab.com:13841/memo');﻿
+    var db = mongoose.connection;
+    db.once("open", function(){
+     console.log("DB connected");
+    });
+    db.on("error", function(err){
+     console.log("DB ERROR : ", err);
+    });
+
+    // Other settings
+    app.set("view engine", "ejs");
+    app.use(express.static(__dirname+"/public"));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(methodOverride("_method"));
+
+    // Routes
+    app.use("/", require("./routes/home"));
+    app.use("/posts", require("./routes/posts"));
+    app.use("/users", require("./routes/users"));  // 1
+
+    // listen (start app with node server.js) ======================================
+    app.listen(3000,function(){
+    console.log("App listening on port : 3000");
+  });
